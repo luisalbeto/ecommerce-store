a3fe557a282203527ea0af4eeab29785f531548b
luisalbeto <luis-alberto11@hotmail.com>
2024-01-07 20:10:43 -0400

    Billboards view (Store)

--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -1,8 +1,23 @@
-const HomePage = ( ) => {
+import Container from "@/components/ui/container";
+import Billboard from "@/components/billboard";
+import getBillboard from "@/actions/get-billboard";
+
+export const revalidate = 0;
+
+
+const HomePage = async ( ) => {
+
+  const billboard = await getBillboard("7b319e64-88c5-46e7-93d3-73a8ad247b3c")
   return(
-  <div>
-    Hello Store
-  </div>)
+
+    <Container>
+      <div>
+        
+      </div>
+
+
+    </Container>
+ )
 }
 
 export default HomePage;
